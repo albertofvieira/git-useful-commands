79020d8150140d73bf01b04b797543e3342136af
Alberto Vieira <alberto.vieira@rerum.com.br>
2022-06-01 18:21:27 -0300

    coding

---
+++ README.md
@@ -0,0 +1,3 @@
+# git-useful-commands
+Useful commands for git
+
---
+++ create-aliases.sh
@@ -0,0 +1,15 @@
+# For short or long command with parameters, just create a shortcut
+
+#Examples
+
+# git st
+git config --global alias.st status
+
+# git ada
+git config --global alias.ada 'add .'
+
+# git ca
+git config --global alias.ca 'commit --amend -m "coding"'
+
+# git cf
+git config --global alias.cf 'commit -m "coding"'
